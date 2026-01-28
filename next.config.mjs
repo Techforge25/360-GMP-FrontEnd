f956c48fd0edda4fe8b012e89fbfaf745427bcf6
Techforge25 <github@techforgeinnovations.com>
2026-01-28 12:24:43 +0500

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
     return [
       {
         source: "/api/v1/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`,
+        destination: `http://localhost:8000/api/v1/:path*`,
       },
     ];
   },
