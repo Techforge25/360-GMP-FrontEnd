490569979f0be4306223903ebad99fecbe66f0da
Techforge25 <github@techforgeinnovations.com>
2026-01-28 12:10:52 +0500

    feat: Configure Next.js to allow Cloudinary images and API rewrites, removing export and unoptimized image settings.

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,23 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "res.cloudinary.com",
-//         port: "",
-//         pathname: "/**",
-//       },
-//     ],
-//   },
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/api/v1/:path*",
-//         destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`,
-//       },
-//     ];
-//   },
-// };
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
-    unoptimized: true
-  }
-}
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
