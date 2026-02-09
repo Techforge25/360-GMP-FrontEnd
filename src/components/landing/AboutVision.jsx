76c39b363c13b2bb53549bf17e6c1ae80cf7405c
Techforge25 <github@techforgeinnovations.com>
2026-02-09 16:00:13 +0500

    changes

--- src/components/landing/AboutVision.jsx
+++ src/components/landing/AboutVision.jsx
@@ -16,7 +16,7 @@ const AboutVision = () => {
             <h2 className="text-2xl md:text-3xl font-bold text-gray-900 mb-4">
               About Us
             </h2>
-            <p className="text-base md:text-xl text-gray-700 leading-relaxed mb-6">
+            <p className="text-base  text-gray-700 leading-relaxed mb-6">
               360 Global Marketplace is dedicated to revolutionizing the way
               businesses connect with suppliers and customers. Our platform
               provides a seamless and efficient way to facilitate mutually
@@ -24,7 +24,7 @@ const AboutVision = () => {
               parties involved.
             </p>
 
-            <p className="text-base md:text-xl text-gray-700 leading-relaxed mb-6">
+            <p className="text-base  text-gray-700 leading-relaxed mb-6">
               By joining 360 Global Marketplace LLC, you're taking a significant
               step towards empowering businesses and creating a more equitable
               and connected global business community.
@@ -35,7 +35,7 @@ const AboutVision = () => {
                 src="/assets/images/rangeDot.png"
                 alt=""
               />
-              <ul className="space-y-[14px] mb-6 text-left inline-block">
+              <ul className="space-y-[14px] ml-4 mb-6 text-left inline-block">
                 {[
                   "Corp to Corp",
                   "Corp to Business",
@@ -45,7 +45,7 @@ const AboutVision = () => {
                 ].map((item, i) => (
                   <li
                     key={i}
-                    className="flex items-center gap-2 text-base md:text-xl text-gray-800"
+                    className="flex items-center gap-2 text-base  text-gray-800"
                   >
                     <span className="w-1.5 h-1.5 bg-purple-600 rounded-full flex-shrink-0 sm:hidden" />
                     {item}
@@ -70,7 +70,7 @@ const AboutVision = () => {
             </div>
           </div>
 
-          <div className="w-full md:w-1/2 order-1 md:order-2 flex justify-center">
+          <div className="w-full md:w-1/2 order-1 md:order-2 flex justify-end">
             <div className="relative rounded-2xl overflow-hidden shadow-lg w-full max-w-lg">
               <img
                 src="/assets/images/aboutUs.png"
@@ -92,7 +92,7 @@ const AboutVision = () => {
 
         {/* Vision Section */}
         <div className="flex flex-col md:flex-row items-center gap-8 md:gap-12">
-          <div className="w-full md:w-1/2 flex justify-center">
+          <div className="w-full md:w-1/2 flex">
             <div className="relative rounded-2xl overflow-hidden shadow-lg w-full max-w-md">
               <img
                 src="/assets/images/vision.png"
@@ -106,14 +106,14 @@ const AboutVision = () => {
             <h3 className="text-xl md:text-2xl font-bold text-gray-900 mb-4">
               Vision
             </h3>
-            <p className="text-base md:text-xl text-gray-700 leading-relaxed mb-4">
+            <p className="text-base  text-gray-700 leading-relaxed mb-4">
               360 Global Marketplace is dedicated to revolutionizing the way
               businesses connect with suppliers and customers. Our platform
               provides a seamless and efficient way to facilitate mutually
               beneficial relationships, driving growth and success for all
               parties involved.
             </p>
-            <p className="text-base md:text-xl text-gray-700 leading-relaxed">
+            <p className="text-base  text-gray-700 leading-relaxed">
               360 GLOBAL MARKETPLACE is committed to linking businesses ,
               fostering innovation, and connecting global markets that drive
               sustainable success
@@ -145,12 +145,12 @@ const AboutVision = () => {
             <h3 className="text-xl md:text-4xl font-bold text-black mb-4">
               Mission
             </h3>
-            <p className="text-base md:text-xl text-gray-700 leading-relaxed mb-4">
+            <p className="text-base  text-gray-700 leading-relaxed mb-4">
               At 360 GLOBAL, we are dedicated to helping individuals,
               entreprenuers, businesses and corporations achieve their goals and
               realize their full potential.
             </p>
-            <p className="text-base md:text-xl text-gray-700 leading-relaxed">
+            <p className="text-base  text-gray-700 leading-relaxed">
               <span className="font-bold text-[#240457]">OUR MISSION</span> is
               to provide expert guidance and support to the small business
               owners as well as large corporations. We Bring Your Vision To The
