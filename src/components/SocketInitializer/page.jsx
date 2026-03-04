e9f39f3e41bfd4eded28f240c094f372cdc1feab
Techforge25 <github@techforgeinnovations.com>
2026-03-05 02:06:05 +0500

    Created a client component to initialize

---
+++ src/components/SocketInitializer/page.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect } from "react";
+import { socket } from "@/services/socket";
+
+export default function SocketInitializer() 
+{
+    useEffect(() => {
+        socket.on("connect", () => console.log("Socket connected:", socket.id));
+        socket.on("disconnect", () => console.log("Socket disconnected"));
+
+        return () => {
+            socket.off("connect");
+            socket.off("disconnect");
+        };
+    }, []);
+    return null;
+}
